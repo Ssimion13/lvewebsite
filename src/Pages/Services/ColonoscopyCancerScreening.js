e2c88546466e4a041ffa74f01b2e928352a7c639
ssimion13 <scottsimion@gmail.com>
2018-08-26 23:36:34 -0700

    added in services page

---
+++ src/Pages/Services/ColonoscopyCancerScreening.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function ColonoscopyCancerScreening() {
+    return (
+        <div className="sidePageDiv">
+        </div>
+    )
+} 
+
+export default ColonoscopyCancerScreening;
--- src/Pages/Services/index.js
+++ src/Pages/Services/index.js
@@ -1,9 +1,80 @@
 import React from "react";
+import {Button} from "reactstrap";
+import {Link} from 'react-router-dom';
 
 function Services(){
     return(
-        <div> 
-            Services 
+        <div className="servicesMain"> 
+            <h1> Services </h1> 
+            <div className="serviceDiv">
+                <h2> Colonoscopy and Colon Cancer Screening Services </h2>
+                <p> A colonoscopy is an exam of the entire colon and the rectum with a colonoscope,
+                    a thin flexible tube with a small camera at the end. This painless procedure
+                    can detect abnormalities within the colon such as ulcerations, inflammation,
+                    strictures or narrowing, polyps and neoplasm or cancer.
+                </p>
+                <Link to="/"> 
+                    <Button> Click Here </Button>
+                </Link>
+            </div>
+            <div className="serviceDiv">
+                <h2> Upper Endoscopy - EGD </h2>
+                <p>
+                    An upper endoscopy, also referred to as an esophagogastroduodenoscopy, 
+                    or EGD, is a painless examination of the esophagus, stomach and the 
+                    first portion of the small intestine or the duodenum. This is done 
+                    with an endoscope, a thin, flexible tube with a camera at the end. 
+                    Tissue samples, referred to as biopsies, may be taken during the procedure 
+                    to check for inflammation, changes in the lining of the esophagus, 
+                    stomach and duodenum, and to determine the presence of the bacteria, 
+                    Helicobacter pylori, which can cause gastric cancer.
+                </p>
+                <Link to="/"> 
+                    <Button> Click Here </Button>
+                </Link>
+            </div>
+            <div className="serviceDiv">
+                <h2> Capsule Endoscopy </h2>
+                <p>
+                    Capsule endoscopy is an outpatient procedure that involves visualization 
+                    of the small intestine with the use of a wireless capsule equipped with a 
+                    tiny camera that takes thousands of pictures for later evaluation by the 
+                    gastroenterologist. The procedure, which typically takes approximately 8 hours,
+                    is able to detect abnormalities in the small bowel that the conventional 
+                    endoscope and colonoscope is unable to reach.
+                </p>
+                <Link to="/"> 
+                    <Button> Click Here </Button>
+                </Link>
+            </div>
+            <div className="serviceDiv">
+                <h2> GI Motility Clinic </h2>
+                <p>
+                    Dr. Fajardo and Dr. Cremonini have both undergone specialized training in GI Motility
+                    and Neurogastroenterology Disorders. Las Vegas Gastroenterology provides state-of-the-art 
+                    technology that has the ability to perform certain specialized diagnostic procedures 
+                    for gastrointestinal motility disorders. These procedures help doctors obtain further 
+                    understanding of your diagnosis so you can begin your appropriate treatment.
+                </p>
+                <Link to="/"> 
+                    <Button> Click Here </Button>
+                </Link>
+            </div>
+            <div className="serviceDiv">
+                <h2> Viral Hepatitis Clinic </h2>
+                <p>
+                    Las Vegas Gastroenterology provides care for patients infected with viral 
+                    hepatitis B and C. With comprehensive, in-depth training and years of 
+                    experience in the field of hepatology, our physicians are able to diagnose, 
+                    treat and manage patients who have been infected with the potentially deadly
+                    disease. Left untreated, viral hepatitis can lead to liver cirrhosis and liver 
+                    cancer, as well as other complications that can seriously decrease a person’s
+                    quality of life. so you can begin your appropriate treatment.
+                </p>
+                <Link to="/"> 
+                    <Button> Click Here </Button>
+                </Link>
+            </div>
         </div>
     )
 }
