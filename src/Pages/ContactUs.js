d711eb4fee8d86a1b8056a81f9a13d3bf3187c20
ssimion13 <scottsimion@gmail.com>
2018-08-28 23:26:04 -0700

    got google maps working on the contact page!

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,12 +1,18 @@
 import React from "react";
+import MapWithAMarker from "./MapWithAMarker";
 
-
-function ContactUs(){
+function ContactUs(props){
     return(
-        <div className="informativePage" > 
-            ContactUs 
+        <div className="informativePage">   
+              <MapWithAMarker
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAf-HYZFuFTYA1jYr6Xf5SuqB6PCx_nZVE&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
         </div>
     )
 }
 
 export default ContactUs;
+
