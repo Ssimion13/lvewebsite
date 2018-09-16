077531e1f76ed0b50a29614930c08c2f567c7f07
ssimion13 <scottsimion@gmail.com>
2018-09-16 02:27:32 -0700

    small progress; started work on footer

--- src/Pages/FrontPage/Footer.js
+++ src/Pages/FrontPage/Footer.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
+import logo from "../../Images/logo.png"
 
 
 const TopBar = function(){
         return (
             <div className="footerMain">
                 <div className="footerTextContent">
-                    <div className="footerNavbar">
+                    {/* <div className="footerNavbar">
                         <Link className="footerButton" to="/"> Home </Link>
                         <Link className="footerButton" to="/Services"> Services </Link>
                         <Link className="footerButton" to="/AboutUs"> About </Link>
                         <Link className="footerButton" to="/Awards"> Awards </Link>
                         <Link className="footerButton" to="/ContactUs"> Contact </Link>
-                    </div>
+                    </div> */}
+                    <img className="footerLogo" src={logo} />
+                    <p className="footerAddress">    
+                        Las Vegas Endoscopy <br/>
+                        7315 S Pecos Road <br/>
+                        Suite 103 <br/>
+                        Las Vegas, NV 89120 <br/>
+                    </p> <br/>
+                    <p className="footerAddress">
+                    Phone: (816) 232-2333 
+                    </p>
+                    <h4> Hours of Operation </h4>
+                    <p>
+                        Monday: 6:30am - 4pm <br/>
+                        Tuesday: 6:30am - 4pm <br/>
+                        Wednesday: 6:30am - 4pm <br/>
+                        Thursday: 6:30am - 4pm <br/>
+                        Friday: 6:30am - 4pm <br/>
+                    </p>
                 </div>
+
 
             </div>
         )
