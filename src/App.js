1d3777134c8d888008cea804b497f799d0749c50
ssimion13 <scottsimion@gmail.com>
2018-11-12 00:58:55 -0800

    added loading screen on page load-- designed for mobile first but looks perfectly presentable in desktop

--- src/App.js
+++ src/App.js
@@ -21,36 +21,73 @@ import HydrogenBreathTest from "./Pages/Services/GIMotilityClinic/HydrogenBreath
 import DrFajardo from "./Pages/AboutUs/DrFajardo.js";
 import DrCremonini from "./Pages/AboutUs/DrCremonini.js";
 import Footer from "./Footer.js"
+import logo from "./Images/transparentlogo.png";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      doneLoading: false
+    }
+    this.timer = this.timer.bind(this);
+  }
+  
+  componentDidMount(){
+    this.timer();
+  }
+
+  timer() {
+    setTimeout(() => {
+      this.setState({doneLoading: true})
+    }, 1500);
+  }
+
+
   render() {
-    return (
+    let data;
+    if(this.state.doneLoading) {
+      data =       
       <div className="app">
         <TopBar />
         {/* <Navbar /> */}
         <Switch> 
-          <Route exact path="/" component={FrontPage} /> 
-          <Route path="/AboutUs" component={AboutUs} />
-          <Route path="/Awards" component={Awards} />
-          <Route path="/Location" component={Location} />
-          <Route path="/Services" component={Services} />
-          <Route path="/ColonoscopyCancerScreening" component={ColonoscopyCancerScreening} />
-          <Route path="/UpperEndoscopy" component={UpperEndoscopy} />
-          <Route path="/CapsuleEndoscopy" component={CapsuleEndoscopy} />
-          <Route path="/GIMotilityClinic" component={GIMotilityClinic} />
-          <Route path="/ViralHepatitisClinic" component={ViralHepatitisClinic} />
-          <Route path="/SmartPill" component={SmartPill} />
-          <Route path="/EsophagealManometry" component={EsophagealManometry} />
-          <Route path="/EsophagealPH" component={EsophagealPH} />
-          <Route path="/HPyloriBreathTest" component={HPyloriBreathTest} />
-          <Route path="/HydrogenBreathTest" component={HydrogenBreathTest} />
-          <Route path="/DrFajardo" component={DrFajardo} />
-          <Route path="/DrCremonini" component={DrCremonini} />
+            <Route exact path="/" component={FrontPage} /> 
+            <Route path="/AboutUs" component={AboutUs} />
+            <Route path="/Awards" component={Awards} />
+            <Route path="/Location" component={Location} />
+            <Route path="/Services" component={Services} />
+            <Route path="/ColonoscopyCancerScreening" component={ColonoscopyCancerScreening} />
+            <Route path="/UpperEndoscopy" component={UpperEndoscopy} />
+            <Route path="/CapsuleEndoscopy" component={CapsuleEndoscopy} />
+            <Route path="/GIMotilityClinic" component={GIMotilityClinic} />
+            <Route path="/ViralHepatitisClinic" component={ViralHepatitisClinic} />
+            <Route path="/SmartPill" component={SmartPill} />
+            <Route path="/EsophagealManometry" component={EsophagealManometry} />
+            <Route path="/EsophagealPH" component={EsophagealPH} />
+            <Route path="/HPyloriBreathTest" component={HPyloriBreathTest} />
+            <Route path="/HydrogenBreathTest" component={HydrogenBreathTest} />
+            <Route path="/DrFajardo" component={DrFajardo} />
+            <Route path="/DrCremonini" component={DrCremonini} />
         </Switch>
         <Footer />
         {window.innerWidth < 500 ?
-          <BottomButtons />
+        <BottomButtons />
         : null}
+    </div>
+    } else {
+      data = 
+        <div className="loadingScreen"> 
+          <h2 className="loadingScreenText"> <i> Now Loading </i> </h2>
+          <div className="loadingScreenLogoDiv">
+            <img className="loadingScreenLogo" src={logo} />
+          </div>
+      
+        </div>
+    }
+    return (
+      <div>
+         {data}
       </div>
     );
   }
